f9e70c80c8f984c2ee5f2d1d3f41efe5c52d74d7
Vicente Rodriguez <dev.vrodriguez@gmail.com>
2021-10-01 00:03:48 -0400

    Added semanticUI + initial layout structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import 'semantic-ui-css/semantic.min.css';
+import '../styles/main.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
+const FULL_HEIGHT = '100vh';
+const containerStyle = {
+  minHeight: FULL_HEIGHT,
+  padding: '0 0.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: FULL_HEIGHT
+}
+
+const mainStyle = {
+  flex: 1,
+  padding: '5rem 0',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+const footerStyle = {
+  width: '100%',
+  height: '40px',
+  borderTop: '1px solid #eaeaea',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
 
 export default function Home() {
   return (
-    <div>hello</div>
+    <div style={containerStyle}>
+      <Head>
+        <title>Reddit Client Practice</title>
+        <meta name="description" content="Reddit Client Practice" />
+      </Head>
+
+      <main style={mainStyle}>
+        Reddit Client Practice
+      </main>
+
+      <footer style={footerStyle}>
+        <a
+          href="https://github.com/VrodriguezDev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By Vicente Rodriguez
+        </a>
+      </footer>
+    </div>
   )
 }
